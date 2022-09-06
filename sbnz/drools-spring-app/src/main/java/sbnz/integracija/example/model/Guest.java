72b225485256f1ed06a910660be838507565ddc5
dusan.antic <dusan.antic177@gmail.com>
2022-09-06 22:31:22 +0200

    added guest

--- sbnz/drools-spring-app/src/main/java/sbnz/integracija/example/model/Guest.java
+++ sbnz/drools-spring-app/src/main/java/sbnz/integracija/example/model/Guest.java
@@ -2,7 +2,14 @@ package sbnz.integracija.example.model;
 
 import javax.persistence.Entity;
 
+import sbnz.integracija.example.model.enums.ManufactureType;
+
 @Entity
 public class Guest extends User {
 	
+	private Long energyProduced;
+	
+	private ManufactureType category;
+	
+	private Long discount; 
 }
